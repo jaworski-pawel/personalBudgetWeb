b71904b4756ad14913e2b91ff5336bb8a8c30e7a
Pawel Jaworski <pawel.jaworski@live.com>
2018-07-10 23:01:27 +0200

    Addition of the Bootstrap library

--- js/piechartexpenses.js
+++ js/piechartexpenses.js
@@ -14,8 +14,45 @@ function drawChart() {
   ['Inne wydatki', 5],
 ]);
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'Wydatki', 'width':1000, 'height':500};
+  if(window.innerWidth>992) {
+    var options = {
+      'backgroundColor':'transparent',
+      'title':'Wydatki',
+      'width':1200,
+      'height':500,
+      'legend': {'textStyle': {'color':'#ffffff'}},
+      'titleTextStyle': {'color':'#ffffff'}
+    }
+  }
+  else if(window.innerWidth>768) {
+    var options = {
+      'backgroundColor':'transparent',
+      'title':'Wydatki',
+      'width':900,
+      'height':400,
+      'legend': {'textStyle': {'color':'#ffffff'}},
+      'titleTextStyle': {'color':'#ffffff'}
+    }
+  }
+  else if(window.innerWidth>576) {
+    var options = {
+      'backgroundColor':'transparent',
+      'title':'Wydatki',
+      'width':700,
+      'height':300,
+      'legend': {'textStyle': {'color':'#ffffff'}},
+      'titleTextStyle': {'color':'#ffffff'}
+    }
+  }
+  else {
+     // Optional; add a title and set the width and height of the chart
+     var options = {
+      'backgroundColor':'transparent',
+      'title':'Wydatki',
+      'legend': {'textStyle': {'color':'#ffffff'}},
+      'titleTextStyle': {'color':'#ffffff'}
+    }
+  }
 
   // Display the chart inside the <div> element with id="piechart"
   var chart = new google.visualization.PieChart(document.getElementById('piechartexpenses'));
--- js/piechartincomes.js
+++ js/piechartincomes.js
@@ -12,9 +12,45 @@ function drawChart() {
   ['Inne', 2],
 ]);
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'Przychody', 'width':1000, 'height':500};
-
+if(window.innerWidth>992) {
+  var options = {
+    'backgroundColor':'transparent',
+    'title':'Wydatki',
+    'width':1200,
+    'height':500,
+    'legend': {'textStyle': {'color':'#ffffff'}},
+    'titleTextStyle': {'color':'#ffffff'}
+  }
+}
+else if(window.innerWidth>768) {
+  var options = {
+    'backgroundColor':'transparent',
+    'title':'Wydatki',
+    'width':900,
+    'height':400,
+    'legend': {'textStyle': {'color':'#ffffff'}},
+    'titleTextStyle': {'color':'#ffffff'}
+  }
+}
+else if(window.innerWidth>576) {
+  var options = {
+    'backgroundColor':'transparent',
+    'title':'Wydatki',
+    'width':700,
+    'height':300,
+    'legend': {'textStyle': {'color':'#ffffff'}},
+    'titleTextStyle': {'color':'#ffffff'}
+  }
+}
+else {
+   // Optional; add a title and set the width and height of the chart
+   var options = {
+    'backgroundColor':'transparent',
+    'title':'Wydatki',
+    'legend': {'textStyle': {'color':'#ffffff'}},
+    'titleTextStyle': {'color':'#ffffff'}
+  }
+}
   // Display the chart inside the <div> element with id="piechart"
   var chart = new google.visualization.PieChart(document.getElementById('piechartincomes'));
   chart.draw(data, options);
