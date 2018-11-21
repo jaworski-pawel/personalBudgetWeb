80cec6068aa4911f8203638f570a8e4eab436278
Pawel Jaworski <pawel.jaworski@live.com>
2018-11-21 21:51:22 +0100

    Showing user's expenses and incomes from DB in tables

--- showbalance.php
+++ showbalance.php
@@ -1,3 +1,11 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['user_logged_in'])) {
+		header('Location: index.php');
+		exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="pl_PL">
   <head>
@@ -45,28 +53,51 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Pensja 1</td>
-                            <td>Wynagrodzenie</td>
-                            <td>10-05-2018</td>
-                            <td>1000.00</td>
-                        </tr>
-                        <tr>
-                            <td>Pensja 2</td>
-                            <td>Wynagrodzenie</td>
-                            <td>10-05-2018</td>
-                            <td>1000.00</td>
-                        </tr>
-                        <tr>
-                            <td>Pensja 3</td>
-                            <td>Wynagrodzenie</td>
-                            <td>10-05-2018</td>
-                            <td>1000.00</td>
-                        </tr>
-                        <tr>
-                                <td colspan="3">Razem</td>
-                                <td id="totalincomes">3000.00</td>
-                            </tr>
+                    <?php
+                      require_once "connect.php";
+                      mysqli_report(MYSQLI_REPORT_STRICT);
+                        
+                      try 
+                      {
+                        $db_connection = new mysqli($host, $db_user, $db_password, $db_name);
+                        
+                        if ($db_connection->connect_errno!=0)
+                        {
+                          throw new Exception(mysqli_connect_errno());
+                        }
+                        else
+                        {
+                          if ($query_result = $db_connection->query(sprintf("SELECT i.income_comment, c.name AS category, i.date_of_income, i.amount FROM incomes AS i NATURAL JOIN incomes_category_assigned_to_users AS c WHERE user_id='%s'", mysqli_real_escape_string($db_connection, $_SESSION['id'])))) {
+                            $number_of_incomes = $query_result->num_rows;
+                            if($number_of_incomes > 0) {
+                              while($incomes = $query_result->fetch_assoc()) {
+                                echo '<tr><td>'.$incomes["income_comment"].'</td><td>'.$incomes["category"].'</td><td>'.$incomes["date_of_income"].'</td><td>'.$incomes["amount"];
+                              }
+                            }
+                            else {
+                              $_SESSION['e_payment_methods'] = "Nie ma wydatków do wyświetlenia.";
+                            }
+                            
+                          }
+                          else
+                          {
+                            throw new Exception($db_connection->error);
+                          }
+                          if ($query_result = $db_connection->query(sprintf("SELECT SUM(amount) AS sum_of_incomes FROM incomes WHERE user_id='%s'", mysqli_real_escape_string($db_connection, $_SESSION['id'])))) {
+                            $sum_of_incomes = $query_result->fetch_assoc();  
+                            echo '<tr><td colspan="3">Razem</td><td id="totalincomes">'.$sum_of_incomes["sum_of_incomes"].'</td></tr>';
+                          }
+
+                          
+                          $db_connection->close();
+                        }
+                      }
+                      catch(Exception $e)
+                      {
+                        echo '<div class="error text-center">Błąd serwera! Przepraszamy za niedogodności i prosimy o wizytę w innym terminie!</div>';
+                        //echo '<br />Informacja developerska: '.$e;
+                      }
+                    ?>
                     </tbody>
                 </table>
 
@@ -83,31 +114,51 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Wydatek 1</td>
-                            <td>Gotówka</td>
-                            <td>Wynagrodzenie</td>
-                            <td>10-05-2018</td>
-                            <td>1000.00</td>
-                        </tr>
-                        <tr>
-                            <td>Wydatek 2</td>
-                            <td>Karta debetowa</td>
-                            <td>Wynagrodzenie</td>
-                            <td>10-05-2018</td>
-                            <td>2000.00</td>
-                        </tr>
-                        <tr>
-                            <td>Wydatek 3</td>
-                            <td>Karta kredytowa</td>
-                            <td>Wynagrodzenie</td>
-                            <td>10-05-2018</td>
-                            <td>1000.00</td>
-                        </tr>
-                        <tr>
-                            <td colspan="4">Razem</td>
-                            <td id="totalexpenses">4000.00</td>
-                        </tr>
+                        <?php
+                        require_once "connect.php";
+                        mysqli_report(MYSQLI_REPORT_STRICT);
+                            
+                        try 
+                        {
+                            $db_connection = new mysqli($host, $db_user, $db_password, $db_name);
+                            
+                            if ($db_connection->connect_errno!=0)
+                            {
+                            throw new Exception(mysqli_connect_errno());
+                            }
+                            else
+                            {
+                            if ($query_result = $db_connection->query(sprintf("SELECT e.expense_comment, ( SELECT c.name FROM expenses_category_assigned_to_users AS c WHERE c.id = e.expense_category_assigned_to_user_id ) AS category, ( SELECT p.name FROM payment_methods_assigned_to_users AS p WHERE p.id = e.payment_method_assigned_to_user_id ) AS payment, e.date_of_expense, e.amount FROM expenses AS e WHERE user_id = '%s'", mysqli_real_escape_string($db_connection, $_SESSION['id'])))) {
+                                $number_of_expenses = $query_result->num_rows;
+                                if($number_of_expenses > 0) {
+                                while($expenses = $query_result->fetch_assoc()) {
+                                    echo '<tr><td>'.$expenses["expense_comment"].'</td><td>'.$expenses["payment"].'</td><td>'.$expenses["category"].'</td><td>'.$expenses["date_of_expense"].'</td><td>'.$expenses["amount"];
+                                }
+                                }
+                                else {
+                                $_SESSION['e_payment_methods'] = "Nie ma wydatków do wyświetlenia.";
+                                }
+                                
+                            }
+                            else
+                            {
+                                throw new Exception($db_connection->error);
+                            }
+                            if ($query_result = $db_connection->query(sprintf("SELECT SUM(amount) AS sum_of_expenses FROM expenses WHERE user_id='%s'", mysqli_real_escape_string($db_connection, $_SESSION['id'])))) {
+                                $sum_of_incomes = $query_result->fetch_assoc();  
+                                echo '<tr><td colspan="4">Razem</td><td id="totalexpenses">'.$sum_of_incomes["sum_of_expenses"].'</td></tr>';
+                            }
+
+                            
+                            $db_connection->close();
+                            }
+                        }
+                        catch(Exception $e)
+                        {
+                            echo '<div class="error text-center">Błąd serwera! Przepraszamy za niedogodności i prosimy o wizytę w innym terminie!</div>';
+                            //echo '<br />Informacja developerska: '.$e;
+                        }
+                        ?>
                     </tbody>
                 </table>
 
@@ -115,7 +166,7 @@
                 <div class="text-center" id="summary"></div>
                 <div class="text-center" id="comment"></div>
                 <div class="text-center col-sm-offset-4 col-sm-4">
-                    <a href="mainmenu.html"><button type="button" class="btn btn-default btn-lg btn-block">Menu główne</button></a>
+                    <a href="mainmenu.php"><button type="button" class="btn btn-default btn-lg btn-block">Menu główne</button></a>
                 </div>
             </div>
         </div>
